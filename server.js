46e60bb72c2bf236b81f90f23b3ad22961c877a3
saicharancherry <saicharancherry593@gmail.com>
2017-02-04 09:38:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/life1',function(req,res){
+    res.send('this is my life opened');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
