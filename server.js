fad520f08a6431b4f63d84cdf6e78c9bd746ea98
saicharancherry <saicharancherry593@gmail.com>
2017-02-05 09:09:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,52 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var content;
+var articleone={
+    title:'article-one',
+    heading:'article One',
+    date:'sep 5 2017',
+    content:`
+    <p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p>
+<p>i am charan .this is my first webapp</p> `
+};
+function htmlfunction(data)
+{
+    var date=data.date;
+    var title=data.title;
+    var content=data.content;
+var htmltemplate=`<html>
+    <body>
+        <link href='/ui/style.css'/>
+        <div>
+<title>
+$(title)
+</title>
+<div class='container'>
+    <a href="/">home</a>
+    <hr/>
+    </div>
+    <div>
+        $(date)
+        </div>
+        <div>
+       <h3>awesome</h3>  
+        </div>
+       
+<h5><p>$(content)
+</div>
+</body>
+</html>`;
+return htmltemplate;
 
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -12,7 +57,7 @@ app.get('/life1',function(req,res){
     res.send('this is my life opened');
 });
 app.get('/article-one',function(req,res){
-  res.sendFile(path.join(__dirname,'ui','article-one.html'));
+ res.send(htmlfunction(articleone));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
