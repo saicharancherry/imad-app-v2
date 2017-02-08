19a1ba62945aeb913825e6c0128fa402dba0ec07
saicharancherry <saicharancherry593@gmail.com>
2017-02-08 06:29:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,15 @@
 console.log('Loaded!');
-var textelement=document.getElementById('textid');
+/*var textelement=document.getElementById('textid');
 textelement.onclick =function()
 {
     textelement.style.marginLeft='100px';
+};*/
+function moveright()
+{
+    moverightreal=moverightreal+10;
+    textelement.style.marginLeft='moverightreal'+'px';
+}
+var textelement=document.getElementById('textid');
+textelement.onclick=function(){
+  var move=setInterval(moveright,100);  
 };
