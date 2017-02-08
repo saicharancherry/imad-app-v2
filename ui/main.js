18a718d3b73363dad81429233fb9f6e4f9ff13e9
saicharancherry <saicharancherry593@gmail.com>
2017-02-08 20:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ textelement.onclick =function()
 var moverightreal=0;
 function moveright()
 {
-    moverightreal=moverightreal+10;
+    moverightreal=moverightreal+1;
     textelement.style.marginLeft=moverightreal+'px';
 }
 var textelement=document.getElementById('textid');
 textelement.onclick=function(){
-  var move=setInterval(moveright,100);  
+  var move=setInterval(moveright,50);  
 };
